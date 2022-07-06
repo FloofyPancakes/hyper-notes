4f4be05b2382132fcb4b15aa2eb13b7471ccfaf7
lij47 <lij47@rpi.edu>
2022-07-06 15:33:46 -0400

    created navbar with buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,30 @@
-import logo from '../logo.svg';
-import NavbarBrand from 'react-bootstrap/esm/NavbarBrand';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/esm/Container';
 import Navbar from 'react-bootstrap/Navbar'
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import { Navbar, Nav, Container } from 'react-bootstrap';
+import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
+import NavDropdown from 'react-bootstrap/esm/NavDropdown';
+import Nav from 'react-bootstrap/esm/Nav';
+import Button from 'react-bootstrap/Button'
+import { BsPlus} from "react-icons/bs";
+import { BsArrowUpRight} from "react-icons/bs";
+import { BsFillFileEarmarkFill } from "react-icons/bs";
+import { BsFillTerminalFill } from "react-icons/bs";
 function Header() {
   return (
-    <Navbar expand="lg" variant="dark" bg="dark">
+    <Navbar expand="lg" bg="light" variant="light">
       <Container>
-        <Navbar.Brand href="#">Navbar</Navbar.Brand>
+        <Navbar.Brand href="#home">Hyper Notes</Navbar.Brand>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Button variant="outline-secondary" > <BsPlus/> </Button>{' '}
+            <Button variant="outline-secondary"> <BsArrowUpRight/> </Button>
+            <Button variant="outline-secondary"> <BsFillFileEarmarkFill/> </Button>
+            <Button variant="outline-secondary"> <BsFillTerminalFill/> </Button>
+          </Nav>
+        </Navbar.Collapse>
+        
       </Container>
+
     </Navbar>
   );
 }
