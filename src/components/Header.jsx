363b30a198166a66374017415291fb4033bb7f44
lij47 <lij47@rpi.edu>
2022-07-17 15:07:24 -0400

    added dropdown menu to settings button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,8 @@ import Container from 'react-bootstrap/esm/Container';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/esm/Nav';
 import Button from 'react-bootstrap/Button'
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 import { BsDiagram2Fill, BsPlus} from "react-icons/bs";
 import { BsArrowUpRight} from "react-icons/bs";
 import { BsFillFileEarmarkFill } from "react-icons/bs";
@@ -28,8 +30,22 @@ function Header() {
 
           </Nav>
         </Navbar.Collapse>
-        
-        <Button variant="outline-secondary"> <BsGearFill className="mb-1"/> </Button>
+        <DropdownButton
+          variant="outline-secondary"
+          // bsStyle="default"
+          // title="No caret"
+          title={<BsGearFill className="mb-1"/>}
+          caret
+          id="dropdown-no-caret"
+        >
+          <Dropdown.Item eventKey="1">Action 1</Dropdown.Item>
+          <Dropdown.Item eventKey="2">Action 2</Dropdown.Item>
+          <Dropdown.Item eventKey="3">Action 3</Dropdown.Item>
+          <Dropdown.Item eventKey="4">Action 4</Dropdown.Item>
+          <Dropdown.Item eventKey="5">Action 5</Dropdown.Item>
+          <Dropdown.Item eventKey="6">Action 6</Dropdown.Item>
+        </DropdownButton>
+
       </Container>
 
     </Navbar>
