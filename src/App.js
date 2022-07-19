e934c8a42b7d635a59cf6517fc2103213cba5f61
qiud1 <qdavid472@gmail.com>
2022-07-19 15:43:10 -0400

    Add sidebar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React, { Component } from 'react';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+
+function App() {
+  return (
+    <><Header /> <Sidebar /></>
+    // <div>
+    //   <Header />
+    // </div>
+  );
+}
+
+export default App;
